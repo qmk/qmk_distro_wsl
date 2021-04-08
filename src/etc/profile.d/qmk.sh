c70b52a2bb9805489b6ef3a0c6de44f1b0c51776
zvecr <git@zvecr.com>
2021-04-08 21:11:33 +0100

    Update welcome color

--- src/etc/profile.d/qmk.sh
+++ src/etc/profile.d/qmk.sh
@@ -1,10 +1,10 @@
 maybe_qmk_welcome () {
+  local RED='\033[0;31m'
   local YELLOW='\033[1;33m'
-  local GREEN='\033[0;32m'
   local NC='\033[0m' # No Color
 
   if [ "$(qmk config user.hide_welcome | grep -c '=True')" == "0" ]; then
-    echo -e "${GREEN}Welcome to QMK WSL!${NC}"
+    echo -e "${RED}Welcome to QMK WSL!${NC}"
     echo -e "  * Documentation:    https://docs.qmk.fm"
     echo -e "  * Support:          https://discord.gg/Uq7gcHh"
     echo -e "  * Quickstart:       Run '${YELLOW}qmk-admin${NC}'"
